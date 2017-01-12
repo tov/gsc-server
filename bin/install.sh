fd099e87739623b67a7b7871e1aea0fd66737c96
Jesse A. Tov <jesse.tov@gmail.com>
2017-01-11 21:55:52 -0600

    Doing a release build for installation now.

--- bin/install.sh
+++ bin/install.sh
@@ -2,9 +2,13 @@
 
 cd "$(dirname $0)"/..
 
-mkdir -p build
-make -C build gsc-fcgi
+mkdir -p build.release
+(
+cd build.release
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make gsc-fcgi
+)
 
 rm -f server_root/gsc
 
-sudo install -o gsc -m 4555 build/gsc-fcgi server_root/gsc
+sudo install -o gsc -m 4555 build.release/gsc-fcgi server_root/gsc
