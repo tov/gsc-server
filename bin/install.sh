ef305e10e1dedce78bb577d0fc57b4f66007b84c
Jesse A. Tov <jesse.tov@gmail.com>
2017-01-11 18:03:50 -0600

    Simplified install.sh by using install command.

--- bin/install.sh
+++ bin/install.sh
@@ -3,12 +3,8 @@
 cd "$(dirname $0)"/..
 
 mkdir -p build
-# make -C build gsc-fcgi
-rm -f server_root/gsc-fcgi
-cp build/gsc-fcgi server_root/
+make -C build gsc-fcgi
 
-(
-cd server_root
-sudo chown gsc:gsc gsc-fcgi
-sudo chmod 6555 gsc-fcgi
-)
+rm -f server_root/gsc-fcgi
+
+sudo install -o gsc -m 4555 build/gsc-fcgi server_root/
