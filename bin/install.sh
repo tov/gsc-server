573e97510e76529e3ebf2ec477f1b6ed850438c2
Jesse A. Tov <jesse.tov@gmail.com>
2017-01-11 19:57:47 -0600

    FCGI works!

--- bin/install.sh
+++ bin/install.sh
@@ -5,6 +5,6 @@ cd "$(dirname $0)"/..
 mkdir -p build
 make -C build gsc-fcgi
 
-rm -f server_root/gsc-fcgi
+rm -f server_root/gsc
 
-sudo install -o gsc -m 4555 build/gsc-fcgi server_root/
+sudo install -o gsc -m 4555 build/gsc-fcgi server_root/gsc
