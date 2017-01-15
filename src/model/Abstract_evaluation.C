8dd13c9570ad14b3bbf76da67dff4c60fd1f93b8
Jesse A. Tov <jesse.tov@gmail.com>
2017-01-15 09:43:41 -0600

    Abstract_evaluation::score_string() const.

--- src/model/Abstract_evaluation.C
+++ src/model/Abstract_evaluation.C
@@ -1,8 +1,14 @@
 #include "Abstract_evaluation.h"
 
+#include "Eval_item.h"
 #include "Submission.h"
 
 const dbo::ptr<Assignment>& Abstract_evaluation::assignment() const
 {
     return submission()->assignment();
+}
+
+std::string Abstract_evaluation::score_string() const
+{
+    return eval_item()->format_score(score());
 }
