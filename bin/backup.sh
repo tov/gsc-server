abb84c55edb5816d9e4ba021b1f7777863593fc3
Jesse A. Tov <jesse.tov@gmail.com>
2017-01-16 15:46:33 -0600

    database backup script

---
+++ bin/backup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+filename=$(date +%Y%m%d-%H%M%S).bz2
+host=login.eecs.northwestern.edu
+dir=eecs211-wi17-backup
+
+rm -f $filename
+sudo -u gsc pg_dump gsc | bzip2 > $filename
+
+ssh $host mkdir -p $dir
+scp $filename $host:$dir/
+
+rm -f $filename
