2b3198d7061ef4c2f1d4b345e24c1874f7e91101
Jesse A. Tov <jesse.tov@gmail.com>
2017-02-08 13:12:25 -0600

    Fixed schema to match current names.

--- schema/add_exam_grade.sql
+++ schema/add_exam_grade.sql
@@ -2,7 +2,7 @@ create table "exam_grades" (
   "id" bigserial primary key ,
   "version" integer not null,
   "user_id" bigint,
-  "sequence" integer not null,
+  "number" integer not null,
   "points" integer not null,
   "possible" integer not null
 )
