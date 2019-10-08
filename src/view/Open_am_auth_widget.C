c41edd12255625adf314e970f23db48f61bc04c0
Jesse A. Tov <jesse.tov@gmail.com>
2019-10-08 14:16:42 -0500

    removed logging crap.

--- src/view/Open_am_auth_widget.C
+++ src/view/Open_am_auth_widget.C
@@ -12,48 +12,6 @@
 #include <string>
 #include <sstream>
 
-
-// TODO DELETE {
-#include "../common/env_var.h"
-#include <cstdlib>
-#include <fstream>
-#include <Wt/WEnvironment>
-struct say
-{
-public:
-    say() = default;
-    //
-    template<class T>
-    say(T const& v)
-    {
-        *this << v;
-    }
-    //
-    template<class T>
-    say& operator<<(T const& v)
-    {
-        buf_ << v;
-        return *this;
-    }
-    //
-    ~say() noexcept(false)
-    {
-        std::cerr << "***** ";
-        std::cerr << buf_.str();
-        std::cerr << "\n";
-    }
-    //
-private:
-    std::ostringstream buf_;
-};
-void header(std::string const& name) {
-    auto const& env = Wt::WApplication::instance()->environment();
-    auto value = env.headerValue(name);
-    say() << "HEADER: " << name << ": " << value;
-};
-// TODO DELETE }
-
-
 using namespace Wt;
 namespace dbo = Wt::Dbo;
 
@@ -98,19 +56,12 @@ void Open_am_auth_widget::reload()
 
     dbo::Transaction transaction(session_);
 
-    say("MARK 0");
     if (session_.authenticate_from_environment()) {
-        say("MARK 1");
         auto div = addNew<WTemplate>(LOGGED_IN_TEMPLATE);
-        say("MARK 2") << session_.user_name();
         div->bindNew<WText>("name", session_.user_name());
-        say("MARK 3");
         div->bindNew<WPushButton>("logout", "Logout")->clicked().connect([&]() {
             WApplication::instance()->redirect(LOGOUT_URL);
         });
-
-        say("MARK 4");
-
     } else {
         auto div = addNew<WTemplate>(LOGGED_OUT_TEMPLATE);
         div->bindNew<WPushButton>("login", "Login")->clicked().connect([&]() {
