91b646afabc9e3d6a5d8bd7f18918d2c95d29f0b
Jesse A. Tov <jesse.tov@gmail.com>
2019-10-08 00:46:40 -0500

    releasing new names

--- bin/release.sh
+++ bin/release.sh
@@ -5,13 +5,13 @@ cd "$(dirname $0)"/..
 # Require password up front
 sudo true
 
-bin/build.sh release
+bin/build.sh release gscd-fcgi gsc-auth
 
 git ls-files server_root | xargs chmod a+r
 
 sudo install -v -o gsc -m 4555 \
     build.release/gscd-fcgi server_root/gscd.fcgi
 sudo install -v -o gsc -m 4555 \
-    build.release/gscauth server_root/gscauth
+    build.release/gsc-auth server_root/gsc-auth
 
 sudo service apache2 restart
