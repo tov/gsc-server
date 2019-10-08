a9f793527cef8391861f9e65274c87e25277dfe1
Jesse A. Tov <jesse.tov@gmail.com>
2019-10-08 00:23:23 -0500

    building via cmake

--- bin/build.sh
+++ bin/build.sh
@@ -7,26 +7,19 @@ cd ..
 case "$1" in
     debug)
         cmake_type=Debug
-        target=gscd
         ;;
     release)
         cmake_type=Release
-        target=gscd-fcgi
         ;;
     *)
-        echo >&2 "Usage: $0 [ debug | release ]"
+        echo >&2 "Usage: $0 [ debug | release ] TARGETS..."
         exit 1
         ;;
 esac
 
 build_dir=build.$1
+shift
 
-if [ ! -d $build_dir ]; then
-    mkdir $build_dir
-    (
-    cd $build_dir
-    cmake -DCMAKE_BUILD_TYPE=$cmake_type ..
-    )
-fi
+cmake -S . -B $build_dir -DCMAKE_BUILD_TYPE=$cmake_type
 
-make -j 4 -C $build_dir $target gscauth
+cmake --build $build_dir --target "$@" -- -j 4
