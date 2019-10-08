ba570d8592b4d9792204fa21398226f2cfabfcd8
Jesse A. Tov <jesse.tov@gmail.com>
2019-10-07 23:11:48 -0500

    Build environment for Clang 8.

--- bin/setup-build-env.sh
+++ bin/setup-build-env.sh
@@ -4,11 +4,14 @@ unset CC
 unset CXX
 unset CFLAGS
 unset CXXFLAGS
+unset LDFLAGS
 
 case "$(hostname)" in
     stewie)
-        CC=gcc-8
-        CXX=g++-8
+        CC=clang-8
+        CXX=clang++-8
+        CXXFLAGS=-stdlib=libc++
+        LDFLAGS=-lc++abi
         ;;
 esac
 
@@ -16,4 +19,4 @@ export CC
 export CXX
 export CFLAGS
 export CXXFLAGS
-
+export LDFLAGS
