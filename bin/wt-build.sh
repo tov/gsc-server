54f75c699cd31b72702f13fb7f79a9c3eef0a35b
Jesse A. Tov <jesse.tov@gmail.com>
2019-10-08 00:23:09 -0500

    building entirely via cmake

--- bin/wt-build.sh
+++ bin/wt-build.sh
@@ -2,15 +2,19 @@
 
 . "${0%/*}/setup-build-env.sh"
 
+set -e
+
+sudo true
+
 build_dir=build.release
 
-if [ ! -d $build_dir ]; then
-    mkdir $build_dir
-    (
-    cd $build_dir
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 ..
-    )
-fi
+cmake -S . -B $build_dir \
+    -DCONNECTOR_HTTP=Off \
+    -DBUILD_EXAMPLES=Off \
+    -DBUILD_TESTS=Off \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=17
 
-cmake --build $build_dir -- -j4
+cmake --build $build_dir -- -j 4
 
+sudo cmake --build $build_dir --target install
