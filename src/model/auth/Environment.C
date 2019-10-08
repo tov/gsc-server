f75dd99a574f599108fb96a9f5a0447dc43322cb
Jesse A. Tov <jesse.tov@gmail.com>
2019-10-08 14:10:15 -0500

    Can acquire authentication from headers.

--- src/model/auth/Environment.C
+++ src/model/auth/Environment.C
@@ -1,22 +1,40 @@
 #include "Environment.h"
 #include "../../common/env_var.h"
 
+#include "Wt/WApplication.h"
+#include "Wt/WEnvironment.h"
+
 namespace {
 
-std::string const open_am_auth_var   = "AUTH_TYPE";
-std::string const open_am_auth_value = "OpenAM";
-std::string const open_am_user_var   = "REMOTE_USER";
+struct Param
+{
+    std::string hdr, var;
+    std::string lookup() const;
+};
+
+Param const auth_type_param   { "X-GSC-Auth-Type",   "AUTH_TYPE" };
+Param const remote_user_param { "X-GSC-Remote-User", "REMOTE_USER" };
+
+std::string const open_am_auth_type = "OpenAM";
+
+std::string Param::lookup() const
+{
+    auto const& env = Wt::WApplication::instance()->environment();
+    auto value = env.headerValue(hdr);
+    return value.empty() ? get_env_var(var) : value;
+}
 
 }
 
 std::optional<std::string> env_remote_user()
 {
 #ifdef GSC_AUTH_OPEN_AM
-    if (get_env_var(open_am_auth_var) != open_am_auth_value)
+    if (auth_type_param.lookup() != open_am_auth_type)
         return {};
 
-    if (auto username = get_env_var(open_am_user_var, nullptr))
-        return std::make_optional<std::string>(username);
+    if (auto username = remote_user_param.lookup();
+            ! username.empty())
+        return std::make_optional(username);
 #endif // GSC_AUTH_OPEN_AM
 
     return {};
