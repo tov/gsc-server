657646eef6438776b01181fe4492b99942eec00a
Jesse A. Tov <jesse.tov@gmail.com>
2020-01-17 19:04:06 -0600

    Added some userinfo::map stuff.

--- src/model/User_info.C
+++ src/model/User_info.C
@@ -1,11 +1,19 @@
 #include "User_info.h"
 #include "auth/User.h"
+#include "../common/util.h"
 
 DBO_INSTANTIATE_TEMPLATES(User_info)
 
 User_info::User_info(const dbo::ptr<User>& user)
         : user_(user)
-{
+{ }
 
+bool User_info::Map::contains(string const& key) const
+{
+    return map_.find(key) != map_.end();
 }
 
+void User_info::Map::remove(std::string const& key)
+{
+    map_.erase(key);
+}
