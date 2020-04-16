f4155408e3c27da37bc479d13c710305929dd3e1
Jesse A. Tov <jesse.tov@gmail.com>
2020-04-16 09:28:27 -0500

    better backup script

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,13 +1,34 @@
 #!/bin/sh
 
-filename=$(date +%Y%m%d-%H%M%S).bz2
+set -e
+
+dir=gsc-backups
+localdir=$HOME/$dir
+filename=cs211-$(date +%Y%m%d-%H%M%S)
 host=login.eecs.northwestern.edu
-dir=gsc-backup
+gpg_recipient=jesse.tov@gmail.com
+gpg_public_key=0xC5668BA5047AE6BD
 
-rm -f $filename
-sudo -u gsc pg_dump gsc | bzip2 > $filename
+gpg_encrypt () {
+    gpg --recipient "$gpg_recipient" \
+        --trusted-key "$gpg_public_key" \
+        --enable-progress-filter \
+        --encrypt "$1"
+}
 
-ssh $host mkdir -p $dir
-scp $filename $host:$dir/
+mkdir -p $localdir
+cd $localdir
 
-rm -f $filename
+echo>&2 Dumping database:
+sudo -u gsc pg_dump gsc > $filename.psql
+echo>&2 Encrypting:
+gpg --sign -z 5 --encrypt --recipient "$gpg_recipient" \
+    --output $filename.psql.bz2.gpg $filename.psql
+# echo>&2 Compressing:
+# bzip2 -v $filename.psql
+# echo>&2 Encrypting:
+# gpg --encrypt 
+rm $filename.psql
+
+echo>&2 Synchronizing:
+rsync --progress -av $localdir $host:
