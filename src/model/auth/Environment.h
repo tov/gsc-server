60c1b1c2e52a668541312f8615284de23fb4d83b
Jesse A. Tov <jesse.tov@gmail.com>
2020-04-16 10:25:12 -0500

    Become_user works better?

--- src/model/auth/Environment.h
+++ src/model/auth/Environment.h
@@ -10,6 +10,10 @@ std::optional<std::string> env_remote_user();
 // Redirects to here + "?whoami={username}"
 void redirect_with_whoami(std::string const& username);
 
+// Redirects to "{uri}?whoami={username}"
+void redirect_with_whoami(std::string const& uri,
+                          std::string const& username);
+
 // Gets the contents of the `whoami` parameter set by
 // `redirect_with_whoami`.
 std::optional<std::string> param_whoami();
