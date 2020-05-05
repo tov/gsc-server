f2e9e5c893a3a969468a7c4c64d0a8411a686310
Jesse A. Tov <jesse.tov@gmail.com>
2020-05-05 15:24:09 -0500

    Make sure to extend into the future.

--- bin/autoextend.sh
+++ bin/autoextend.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+log=/var/log/autoextend.log
+date >> $log
+
 psql -q -c "
     UPDATE submission
-    SET eval_date = eval_date + INTERVAL '24 hrs'
+    SET eval_date = GREATEST(eval_date, utc_now()) + INTERVAL '24 hrs'
     WHERE submission_size(id) < assignment_size(assignment_number)
       AND effective_eval_date(id) < utc_now() + INTERVAL '12 hrs';
 "
--- sql/extend_incomplete_self_eval.sql
+++ sql/extend_incomplete_self_eval.sql
@@ -1,6 +1,6 @@
 UPDATE submission
 SET
-  eval_date = eval_date + INTERVAL '24 hrs'
+  eval_date = GREATEST(eval_date, utc_now()) + INTERVAL '24 hrs'
 WHERE
   submission_size(id) < assignment_size(assignment_number)
 AND
