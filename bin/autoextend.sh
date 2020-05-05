e39b8dadc5d61fd445318833193b7c8f2ad056bf
Jesse A. Tov <jesse.tov@gmail.com>
2020-05-05 14:58:41 -0500

    script to provide automatic self-eval extensions.

---
+++ bin/autoextend.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+psql -q -c "
+    UPDATE submission
+    SET eval_date = eval_date + INTERVAL '24 hrs'
+    WHERE submission_size(id) < assignment_size(assignment_number)
+      AND effective_eval_date(id) < utc_now() + INTERVAL '12 hrs';
+"
