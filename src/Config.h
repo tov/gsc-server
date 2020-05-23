696adae52121b9422e7ffc661c759f84932ce332
Jesse A. Tov <jesse.tov@gmail.com>
2020-05-22 23:40:54 -0500

    turn off showing every query

--- src/Config.h
+++ src/Config.h
@@ -6,7 +6,7 @@ struct Config
 {
     float reward_for_no = 0.2;
 
-    bool show_queries = true;
+    bool show_queries = false;
 
     std::string websso_login_url
             = "https://websso.it.northwestern.edu/amserver/UI/Login";
