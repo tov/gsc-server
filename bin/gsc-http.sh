2d7168be490fbf6e8f10eaa63f031e79bbbe9161
Jesse A. Tov <jesse.tov@gmail.com>
2020-09-28 17:55:27 -0500

    Improve script for starting dev gsc-http.
    Now attempts to change to the correct directory.

--- bin/gsc-http.sh
+++ bin/gsc-http.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-cd "$(dirname $0)/../server_root"
+cd "$(dirname $0)/.."
 
-../build.debug/gscd --docroot html --http-address 0.0.0.0 --http-port 9090
+${dir:-build.debug}/gscd \
+    --docroot server_root/html \
+    --approot server_root \
+    --http-listen 0.0.0.0:9090
