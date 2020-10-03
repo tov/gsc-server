e4d9d1bb0e04fc348c8b773d06582d38aa0dd80e
Jesse A. Tov <jesse.tov@gmail.com>
2020-10-03 14:55:28 -0500

    Make autoextend.sh log more.

--- bin/autoextend.sh
+++ bin/autoextend.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-log=/var/log/autoextend.log
-date >> $log
+# Extend every incomplete self evaluation that’s due in 12 hours or less
+# by at least 24 hours and to at least 24 hours from now.
 
-psql -q -c "
+log=/var/log/autoextend.log
+exec >>$log 2>&1
+
+date
+
+psql -c "
     UPDATE submission
     SET eval_date = GREATEST(eval_date, utc_now()) + INTERVAL '24 hrs'
     WHERE submission_size(id) < assignment_size(assignment_number)
       AND effective_eval_date(id) < utc_now() + INTERVAL '12 hrs';
 "
+
