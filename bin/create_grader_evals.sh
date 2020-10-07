7f6ba5fba72a019f90da4231da03cec1ce73e05c
Jesse A. Tov <jesse.tov@gmail.com>
2020-10-07 17:18:24 -0500

    script to create missing grader_evals

---
+++ bin/create_grader_evals.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Create a grader_eval for every self evaluation of the specified eval
+# item.
+if ! (( $# )); then
+    echo>&2 "Usage: $0 EVAL_ITEM_ID [GRADER_ID [SCORE [COMMENT]]]"
+    exit 1
+fi
+
+eval_item_id=$1
+grader_id=${2:-1}
+score=${3:-1}
+comment="$(printf '%s\n' "${4-}" | sed "s/'/''/g")"
+
+sudo -u gsc psql -c "
+    INSERT INTO grader_eval (
+        version,
+        self_eval_id,
+        grader_id,
+        status,
+        explanation,
+        score,
+        time_stamp
+    ) SELECT
+        0,
+        self_eval.id,
+        $grader_id,
+        2,
+        '$comment',
+        $score,
+        utc_now()
+    FROM self_eval
+        LEFT OUTER JOIN grader_eval ON self_eval_id = self_eval.id
+    WHERE eval_item_id = $eval_item_id
+      AND grader_eval.id IS NULL;
+"
