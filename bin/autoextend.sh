bd231ff49c87ddfb1c6a8eba186c65514bd40b3e
Jesse A. Tov <jesse.tov@gmail.com>
2021-01-25 16:51:06 -0600

    Support doing multiple autoextensions at once.

--- bin/autoextend.sh
+++ bin/autoextend.sh
@@ -7,11 +7,14 @@ log=/var/log/autoextend.log
 exec >>$log 2>&1
 
 echo
-echo "$(date '+%Y-%m-%d %H:%M:%S') - ${0##*/} $*"
+date '+%Y-%m-%d %H:%M:%S'
 
-psql "$@" -c "
-    UPDATE submission
-    SET eval_date = GREATEST(eval_date, utc_now()) + INTERVAL '24 hrs'
-    WHERE submission_size(id) < assignment_size(assignment_number)
-      AND effective_eval_date(id) < utc_now() + INTERVAL '12 hrs';
-"
+for db; do
+    printf '%s: ' "$db"
+    psql $db -c "
+UPDATE submission
+SET eval_date = GREATEST(eval_date, utc_now()) + INTERVAL '24 hrs'
+WHERE submission_size(id) < assignment_size(assignment_number)
+AND effective_eval_date(id) < utc_now() + INTERVAL '12 hrs';
+    "
+done
